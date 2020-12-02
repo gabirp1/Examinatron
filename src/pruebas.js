17fd8e0f8d31603c0b0dd5af426def8bcce1cb09
Gabi <gari.327@gmail.com>
2020-12-02 13:18:23 +0100

    mas

--- src/pruebas.js
+++ src/pruebas.js
@@ -6,7 +6,13 @@ const getHash = async (password)=>{
     const hash = await bcrypt.hash (password, 8)
     console.log(hash);
 }
-
+console.log('password:', password);
 getHash(password)
-console.log(password);
-console.log(getHash(password));
+
+const isRight = async (password)=>{ 
+    const hash = await bcrypt.compare('123', getHash('123456'))
+console.log(compare);
+return true 
+}
+
+isRight(password)
